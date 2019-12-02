bc56e0e215aa3b22b9dab1be2d1f520050a14fb6
Jason Schweier <jasons@nulogy.com>
2019-12-02 08:57:40 -0500

    Day 2

--- lib/advent_of_code2019.ex
+++ lib/advent_of_code2019.ex
@@ -3,4 +3,11 @@ defmodule AdventOfCode2019 do
     File.read!("data/#{day}")
     |> String.split("\n")
   end
+
+  def read_line_of_cs_ints(day) do
+    read_input(day) |> hd |> String.split(",") |> Enum.map(fn x ->
+      {int, ""} = Integer.parse(x)
+      int
+    end)
+  end
 end
