373ef4cba135f4b920a91783c7dc31e051bb3743
Jason Schweier <jasons@nulogy.com>
2019-12-12 21:53:05 -0500

    Day 8, part 2

--- test/day08_test.exs
+++ test/day08_test.exs
@@ -10,9 +10,14 @@ defmodule Day08Test do
         [4,5,6]
       ],
       [
-        [7, 8, 9],
-        [0, 1, 2]
+        [7,8,9],
+        [0,1,2]
       ]
+    ]
+
+    [
+      [[1,7], [2,8], [3,9]],
+      [[4,0], [5,1], [6,2]]
     ]
   end
 
@@ -20,5 +25,21 @@ defmodule Day08Test do
     layers = split_layers(3, 2, "123456789012")
 
     assert check(layers) == 1
+  end
+
+  test "colour" do
+    assert colour([0, 1, 2, 0]) == 0
+    assert colour([2, 1, 2, 0]) == 1
+    assert colour([2, 2, 1, 0]) == 1
+    assert colour([2, 2, 2, 0]) == 0
+  end
+
+  test "decode image" do
+    layers = split_layers(2, 2, "0222112222120000")
+
+    assert decode(2, 2, layers) == [
+      [0, 1],
+      [1, 0]
+    ]
   end
 end
